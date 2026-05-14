32de837890de514237343efe6f2a4ee615c38fca
harshitpatidar08 <harshitpatidar687@gmail.com>
2026-05-14 21:26:11 +0530

    Update route handling for React application

--- server/index.js
+++ server/index.js
@@ -32,9 +32,10 @@ app.use('/api/chatbot', chatbotRouter);
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Route ALL other non-API requests to the React application
-app.get('*', (req, res) => {
+app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
+
 
 // Initialize node-cron
 // Run aiRiskService every 30 minutes
